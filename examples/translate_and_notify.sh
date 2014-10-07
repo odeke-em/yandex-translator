c46e8022733041a3968341e8a129850f4bd37067
Рустам Кашапов <rrkashapov@bars-open.ru>
2014-10-07 21:02:11 +0400

    translate_and_notify script description is added

--- examples/translate_and_notify.sh
+++ examples/translate_and_notify.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+# Translates selected text and shows translation in system notifications area
+# xsel package is required. On Ubuntu it can be installed with
+#   sudo apt-get install xsel
+# command.
+
 notify-send -u critical "Translation:" "$(ytranslate.py "$(xsel -o)")"
